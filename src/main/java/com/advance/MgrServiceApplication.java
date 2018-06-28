39879a9a6ef7290f0047cd50f308212372ddf1b8
huangjian <huangjian@sinter.cn>
2018-06-28 10:25:08 +0800

    first commit

---
+++ src/main/java/com/advance/MgrServiceApplication.java
@@ -0,0 +1,12 @@
+package com.advance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgrServiceApplication.class, args);
+	}
+}
